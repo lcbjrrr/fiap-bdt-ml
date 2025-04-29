dc57b1ea5ee4ae70f2ac304e98e0e6a3ac527b3a
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2025-04-29 14:23:03 -0500

    Update map.py

--- map.py
+++ map.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
-auto = pd.read_csv("https://raw.githubusercontent.com/lcbjrrr/quantai/refs/heads/main/datasets/autoinsurance.csv")
+auto = pd.read_csv("autoinsurance.csv")
 #st.write(df)
 st.map(auto, latitude="lat", longitude="lon", size="2300")
